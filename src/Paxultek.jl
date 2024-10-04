100cf4519a2a63e4ecf95d8fdb37932750731834
Erik Edin <erikedin@users.noreply.github.com>
2024-10-04 21:57:12 +0200

    starting an GLFW window in a separate process

---
+++ src/Paxultek.jl
@@ -0,0 +1,63 @@
+# MIT License
+#
+# Copyright (c) 2024 Erik Edin
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+using GLFW
+using Distributed
+
+struct VisualizerContext
+    channel::RemoteChannel
+    exitchannel::RemoteChannel
+end
+
+function runvisualizer(context::VisualizerContext)
+    # Create a window and its OpenGL context
+    window = GLFW.CreateWindow(640, 480, "GLFW.jl")
+
+    # Make the window's context current
+    GLFW.MakeContextCurrent(window)
+
+    # Loop until the user closes the window
+    while !GLFW.WindowShouldClose(window)
+
+	    # Render here
+
+	    # Swap front and back buffers
+	    GLFW.SwapBuffers(window)
+
+	    # Poll for and process events
+	    GLFW.PollEvents()
+    end
+
+    GLFW.DestroyWindow(window)
+end
+
+function start()
+    channel = RemoteChannel(() -> Channel{Int}(10))
+    exitchannel = RemoteChannel(() -> Channel{Int}(10))
+    context = VisualizerContext(channel, exitchannel)
+
+    workerpid = Distributed.workers()[1]
+
+    remote_do(runvisualizer, workerpid, context)
+
+    context
+end
